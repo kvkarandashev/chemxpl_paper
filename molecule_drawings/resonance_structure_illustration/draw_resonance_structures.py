e3e0ccd04db2e38b9c45d8e67301d437da7af22d
kvkarandashev <kvkarandashev@gmail.com>
2022-11-30 15:39:06 +0100

    Illustration related to importance of resonance structure adjustment.

---
+++ molecule_drawings/resonance_structure_illustration/draw_resonance_structures.py
@@ -0,0 +1,36 @@
+from bmapqml.chemxpl.valence_treatment import str2ChemGraph
+from bmapqml.chemxpl.rdkit_draw_utils import (
+    draw_all_possible_resonance_structures,
+    LIGHTBLUE,
+    LIGHTRED,
+    LIGHTGREEN,
+)
+from bmapqml.chemxpl.rdkit_utils import chemgraph_to_canonical_rdkit
+import numpy as np
+import random
+
+random.seed(1)
+np.random.seed(1)
+
+chemgraph_strings = [
+    "6#3@1:6@2:7@3:6#1@4:6@1@5:8#1",
+    "6@1@5@6@7:16@2:6@3:16@4:6@8@9@10:9:9:9:9:9:9",
+]
+
+kwargs = {
+    "size": (300, 200),
+    "highlightAtomRadius": 0.4,
+    "bw_palette": True,
+    "abbrevs": True,
+}
+
+for str_id, cg_str in enumerate(chemgraph_strings):
+    cg = str2ChemGraph(cg_str)
+
+    SMILES = chemgraph_to_canonical_rdkit(cg, SMILES_only=True)
+
+    print(SMILES)
+
+    file_prefix_cg = "resonance_" + str(str_id) + "_"
+
+    draw_all_possible_resonance_structures(cg, file_prefix_cg, **kwargs)
