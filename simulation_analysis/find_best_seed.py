fb30356432a09c0569299861fd140b19872e0d3f
janweinreich <janweinreich286@gmail.com>
2023-01-08 14:49:08 +0100

    ability to iterate over QM9 and EGP results

--- simulation_analysis/find_best_seed.py
+++ simulation_analysis/find_best_seed.py
@@ -1,16 +1,22 @@
 import sys, os
 from misc_procedures import find_seed_with_best_candidate
+import pdb
+
+simulation_names = ["QM9"]
 
 
-paths = os.listdir("/data/jan/konstantin")
-print(paths)
+for name in simulation_names:
 
-best_seeds = open("best_seeds.txt", "w")
-for p in paths:
-    best = find_seed_with_best_candidate("/data/jan/konstantin/{}".format(p))
-    print(p, best)
-    best_seeds.write("{}\n".format(best))
+    paths = os.listdir("/data/jan/konstantin/{}".format(name))
+    print(paths)
 
-best_seeds.close()
+    best_seeds = open("best_seeds_{}.txt".format(name), "w")
+    for p in paths:
+        #pdb.set_trace()
+        best = find_seed_with_best_candidate("/data/jan/konstantin/{}/{}".format(name,p))
+        print(p, best)
+        best_seeds.write("{}\n".format(best))
+
+    best_seeds.close()
 
 
